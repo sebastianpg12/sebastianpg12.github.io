295fc7280d04651b811d0aba1f9780ddcb55dcc5
Sebastián Pulgarín Gómez <pulgaringomezsebastian@gmail.com>
2020-11-18 18:48:24 -0500

    animaciones

---
+++ index.js
@@ -0,0 +1,57 @@
+window.sr = ScrollReveal();
+  sr.reveal('.topnav', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.perfil_flotante', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-100px'
+  });
+
+
+  window.sr = ScrollReveal();
+  sr.reveal('.container_redes', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-100px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.container_estudios', {
+    duration: 2000,
+    origin: 'left',
+    distance: '-200px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.habilidades ', {
+    duration: 2000,
+    origin: 'left',
+    distance: '-200px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.col-sm-3 ', {
+    duration: 2000,
+    origin: 'left',
+    distance: '-200px'
+  });
+  
+
+  window.sr = ScrollReveal();
+  sr.reveal('.heroe', {
+    duration: 1000,
+    origin: 'top',
+    distance: '-40px'
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.cta-text ', {
+    duration: 1000,
+    origin: 'top',
+    distance: '-40px'
+  });
